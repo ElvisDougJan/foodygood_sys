e378f0cc35468f0735a941918226a131b00b8339
Elvis Douglas Janegitz <edjelvis@gmail.com>
2018-11-07 21:57:01 -0200

    Montando objeto de cadastro de usuário

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app>
-    <v-navigation-drawer persistent v-model="drawer" enable-resize-watcher fixed app>
+    <v-navigation-drawer persistent v-model="drawer" enable-resize-watcher fixed app class="menu-lateral">
       <v-list class="ml-4 mt-3 mr-4">
-        <v-list-group>
+        <v-list-group class="menu-cadastro">
           <v-list-tile slot="activator">
             <v-list-tile-content>
               <v-list-tile-title>Cadastros</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
-          <v-list-tile :to="{name: 'CadastroUsuario'}">
+          <v-list-tile class="opcao-selecionada" :to="{name: 'CadastroUsuario'}">
             <v-list-tile-action>
               <v-icon>person</v-icon>
             </v-list-tile-action>
             <v-list-tile-content>
-              <v-list-tile-title>Usuário</v-list-tile-title>
+              <v-list-tile-title class="text-black">Usuário</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
           <v-list-tile :to="{name: 'CadastroInsumos'}">
@@ -21,7 +21,7 @@
               <v-icon>extension</v-icon>
             </v-list-tile-action>
             <v-list-tile-content>
-              <v-list-tile-title>Insumo</v-list-tile-title>
+              <v-list-tile-title class="text-black">Insumo</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
           <v-list-tile :to="{name: 'CadastroMesas'}">
@@ -29,7 +29,7 @@
               <v-icon>tab</v-icon>
             </v-list-tile-action>
             <v-list-tile-content>
-              <v-list-tile-title>Mesa</v-list-tile-title>
+              <v-list-tile-title class="text-black">Mesa</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
           <v-list-tile :to="{name: 'CadastroItens'}">
@@ -37,7 +37,7 @@
               <v-icon>receipt</v-icon>
             </v-list-tile-action>
             <v-list-tile-content>
-              <v-list-tile-title>Itens</v-list-tile-title>
+              <v-list-tile-title class="text-black">Itens</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
           <v-list-tile :to="{name: CadastroProdutos}">
@@ -45,13 +45,13 @@
               <v-icon>local_dining</v-icon>
             </v-list-tile-action>
             <v-list-tile-content>
-              <v-list-tile-title>Produtos</v-list-tile-title>
+              <v-list-tile-title class="text-black">Produtos</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
         </v-list-group>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar app :clipped-left="clipped">
+    <v-toolbar app :clipped-left="clipped" color="black">
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <!-- <v-btn icon @click.stop="miniVariant = !miniVariant">
           <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
@@ -62,7 +62,7 @@
       <!-- <v-btn icon @click.stop="fixed = !fixed">
           <v-icon>remove</v-icon>
         </v-btn> -->
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-toolbar-title><span style="color:white">Sistema FoodyGood</span></v-toolbar-title>
       <v-spacer></v-spacer>
       <!-- <v-btn icon @click.stop="rightDrawer = !rightDrawer">
           <v-icon>menu</v-icon>
@@ -81,8 +81,8 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
+    <v-footer :fixed="fixed" app style="background-color: black">
+      <span style="color:white">&copy; 2018</span>
     </v-footer>
   </v-app>
 </template>
@@ -107,3 +107,30 @@
     name: 'App'
   }
 </script>
+
+<style scoped>
+  .v-navigation-drawer .v-list {
+    background: transparent !important;
+  }
+  .menu-lateral {
+    background-image: url("/../../static/img/sidebar-img.jpg");
+    background-position: absolute;
+    background-size: cover;
+    background-color: #888 !important;
+    background-blend-mode: multiply;
+  }
+  .menu-cadastro {
+    background-color: #FFCA28;
+    padding: 10px;
+    border-radius: 20px
+  }
+
+  .opcao-selecionada {
+    background-color: #FFF;
+    border-radius: 10px
+  }
+
+  .text-black {
+    color: black
+  }
+</style>
